94ff58e79fa87ff48cd49ef154194f1a1fe8f659
Omkar Date <omkardate29@gmail.com>
2024-01-03 17:08:53 +0530

    fix: Unsplash error removed

--- event-data.js
+++ event-data.js
@@ -1,8 +1,7 @@
 export const eventInformation = [
   {
     title: 'Techfest',
-    image:
-      'https://images.unsplash.com/photo-1492684223066-81342ee5ff30?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    image: '/public/event/event.avif',
     date: '12',
     month: 'Jan',
     time: '12:00-13:10',
@@ -14,8 +13,7 @@ export const eventInformation = [
   },
   {
     title: 'Techfest2',
-    image:
-      'https://images.unsplash.com/photo-1492684223066-81342ee5ff30?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    image: '/public/event/event.avif',
     date: '12',
     month: 'Jan',
     time: '12:00-13:10',
@@ -27,8 +25,7 @@ export const eventInformation = [
   },
   {
     title: 'Techfest2',
-    image:
-      'https://images.unsplash.com/photo-1492684223066-81342ee5ff30?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    image: '/public/event/event.avif',
     date: '12',
     month: 'Jan',
     time: '12:00-13:10',
