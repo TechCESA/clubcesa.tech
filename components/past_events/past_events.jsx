a83bce31a280e6ed97b28f4f2c9fe416179dc27b
rohitbaing08 <rohitbaing456@gmail.com>
2024-01-03 23:36:08 +0530

    fix: Past events cards improved

--- components/past_events/past_events.jsx
+++ components/past_events/past_events.jsx
@@ -9,7 +9,7 @@ import PastEventCard from './past_events_components';
 gsap.registerPlugin(ScrollTrigger);
 
 export default function PastEvents() {
-  const points = [0.08, 0.24, 0.41, 0.59, 0.76];
+  const points = [0.13, 0.5];
   useEffect(() => {
     let leftCards = gsap.utils.toArray('.left-cards');
     leftCards.forEach((card, index) => {
@@ -71,48 +71,27 @@ export default function PastEvents() {
 
   return (
     <div id='pastevents'>
-      <h1 className='my-12 text-4xl font-black'>Past Events</h1>
-      <div className='flex h-[768px] flex-row justify-center'>
-        <div className='flex flex-col pr-[5%] max-sm:hidden'>
-          <div className='left-cards h-1/5'>
+      <h1 className='my-12 text-4xl font-black'>Past&nbsp;&nbsp;Events</h1>
+      <div className='mb-16 flex flex-row justify-center'>
+        <div className='flex flex-col justify-between pr-[5%] max-sm:hidden'>
+          <div className='left-cards my-5'>
             <PastEventCard pastEvents={pastEventInformation[0]} />
           </div>
-          <div className=' h-1/5 max-h-[25%] min-h-[10%]'></div>
-          <div className='left-cards h-1/5'>
-            <PastEventCard pastEvents={pastEventInformation[2]} />
-          </div>
-          <div className=' h-1/5 max-h-[25%] min-h-[10%]'></div>
-          <div className='left-cards h-1/5'>
-            <PastEventCard pastEvents={pastEventInformation[4]} />
-          </div>
+          <div className='shrink grow basis-auto'></div>
         </div>
         <EventPath />
-        <div className='flex flex-col items-start pl-[5%] max-sm:hidden'>
-          <div className=' h-1/5 max-h-[25%] min-h-[10%]'></div>
-          <div className='right-cards h-1/5 '>
+        <div className='flex flex-col justify-between pl-[5%] max-sm:hidden'>
+          <div className='shrink grow basis-auto'></div>
+          <div className='right-cards my-5'>
             <PastEventCard pastEvents={pastEventInformation[1]} />
           </div>
-          <div className=' h-1/5 max-h-[25%] min-h-[10%]'></div>
-          <div className='right-cards h-1/5'>
-            <PastEventCard pastEvents={pastEventInformation[3]} />
-          </div>
-          <div className=' h-1/5 max-h-[25%] min-h-[10%]'></div>
         </div>
-        <div className='flex translate-y-[20px] flex-col justify-between pl-10 sm:hidden'>
-          <div className='right-cards-2 h-1/5 '>
+        <div className='flex flex-col pl-10 sm:hidden'>
+          <div className='right-cards-2 my-5'>
             <PastEventCard pastEvents={pastEventInformation[0]} />
           </div>
-          <div className='right-cards-2 h-1/5 '>
+          <div className='right-cards-2 my-5'>
             <PastEventCard pastEvents={pastEventInformation[1]} />
-          </div>
-          <div className='right-cards-2 h-1/5 '>
-            <PastEventCard pastEvents={pastEventInformation[2]} />
-          </div>
-          <div className='right-cards-2 h-1/5 '>
-            <PastEventCard pastEvents={pastEventInformation[3]} />
-          </div>
-          <div className='right-cards-2 h-1/5 '>
-            <PastEventCard pastEvents={pastEventInformation[4]} />
           </div>
         </div>
       </div>
