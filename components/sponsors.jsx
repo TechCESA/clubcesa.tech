5186131a8aaf11874b82a0d5cda78de7a923a31b
psantosh16 <santoshphadtare1610@gmail.com>
2024-01-05 00:30:15 +0530

    feat: New sponsors design

--- components/sponsors.jsx
+++ components/sponsors.jsx
@@ -4,7 +4,43 @@ export default function Sponsors() {
   return (
     <div>
       <p className='text-center text-4xl font-semibold'>Sponsors</p>
-      <div className='my-10 grid grid-flow-col items-center justify-center justify-items-center gap-10'>
+      <div className='my-10 grid grid-cols-2 items-center justify-center  justify-items-center gap-4 md:grid-flow-col md:grid-cols-1 md:gap-10'>
+        <div className='flex justify-center rounded-2xl border-2 border-white/30 bg-white/80 px-8 py-2'>
+          <Image
+            src={
+              'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flogos-world.net%2Fwp-content%2Fuploads%2F2020%2F10%2FSlack-Logo.png&f=1&nofb=1&ipt=6f67871a50131ff85f1b6fa16001867119bb77c85577dd4fef319bef5680afc8&ipo=images'
+            }
+            width={240}
+            height={320}
+          />
+        </div>
+        <div className='flex justify-center rounded-2xl border-2 border-white/30 bg-white/80 px-8 py-2'>
+          <Image
+            src={
+              'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flogos-world.net%2Fwp-content%2Fuploads%2F2020%2F11%2FShopify-Logo.png&f=1&nofb=1&ipt=700ee4792665166d9c97023d581dc88b798a0355cbe6d2db1f4f4870617052c6&ipo=images'
+            }
+            width={240}
+            height={320}
+          />
+        </div>
+        <div className='flex justify-center rounded-2xl border-2 border-white/30 bg-white/80 px-8 py-2'>
+          <Image
+            src={
+              'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flogos-world.net%2Fwp-content%2Fuploads%2F2020%2F10%2FSlack-Logo.png&f=1&nofb=1&ipt=6f67871a50131ff85f1b6fa16001867119bb77c85577dd4fef319bef5680afc8&ipo=images'
+            }
+            width={240}
+            height={320}
+          />
+        </div>
+        <div className='flex justify-center rounded-2xl border-2 border-white/30 bg-white/80 px-8 py-2'>
+          <Image
+            src={
+              'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flogos-world.net%2Fwp-content%2Fuploads%2F2020%2F11%2FShopify-Logo.png&f=1&nofb=1&ipt=700ee4792665166d9c97023d581dc88b798a0355cbe6d2db1f4f4870617052c6&ipo=images'
+            }
+            width={240}
+            height={320}
+          />
+        </div>
         <div className='flex justify-center rounded-2xl border-2 border-white/30 bg-white/80 px-8 py-2'>
           <Image
             src={
