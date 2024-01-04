b44b92a44f92d99f26f15d2a42b851fdc0520139
Akash Joshi <43512801+Theskyspace@users.noreply.github.com>
2024-01-04 22:22:33 +0530

    chore: Added Varsity

---
+++ app/register/[slug]/page.jsx
@@ -0,0 +1,24 @@
+import Product from '@/components/product/product';
+import Nav from '@/components/product/nav';
+import './product-style.css';
+import { productInformation } from '@/product-data';
+
+const findEvent = (slug) => {
+  return productInformation.find((event) => event.sku.toLowerCase() === slug);
+};
+
+export default function Page({ params }) {
+  let event = findEvent(params.slug);
+
+  if (!event) {
+    event = productInformation[0];
+  }
+
+  return (
+    <>
+      <Nav />
+      {/* <Banner /> */}
+      <Product merch_info={event} />
+    </>
+  );
+}
