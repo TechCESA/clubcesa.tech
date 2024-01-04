ff980aa61d952c4e2067523ee6eda578d895b3e7
Omkar Date <omkardate29@gmail.com>
2024-01-04 13:58:38 +0530

    fix: Footer margin fix

--- components/footer.jsx
+++ components/footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <footer className='mt-8 bg-[#1d1d1d] py-4' id='connect'>
       <div className='m-auto flex max-w-[90vw] flex-col items-center justify-between sm:max-w-[75vw] md:flex-row md:items-start'>
-        <div className='address mb-8 flex flex-col items-center text-center text-white md:block md:text-start'>
+        <div className='address mb-8 flex flex-col items-center text-center text-white md:mb-0 md:block md:text-start'>
           <div className='flex flex-row items-center py-2 font-bold'>
             <Image
               src='/icons/location.png'
@@ -21,7 +21,7 @@ export default function Footer() {
           </div>
         </div>
 
-        <div className='follow mb-8 w-40 text-center text-white'>
+        <div className='follow mb-8 w-40 text-center text-white md:mb-0'>
           <div className='py-2 font-bold'>{`Follow Us`}</div>
           <div className='flex justify-around'>
             <Link href='https://www.instagram.com/cesa.bvcoe/' target='_blank'>
