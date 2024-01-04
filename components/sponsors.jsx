ead97431d11891c86595abfd717454bbe6556348
Omkar Date <omkardate29@gmail.com>
2024-01-05 02:19:31 +0530

    fix: about page

--- components/sponsors.jsx
+++ components/sponsors.jsx
@@ -1,8 +1,7 @@
 export default function Sponsors() {
   return (
     <div>
-      <h1 className='my-12 text-4xl font-black md:text-8xl'>Sponsors ???</h1>
-      <h1 className='my-12 text-4xl font-black md:text-8xl'>Moye Moye</h1>
+      <h1 className='my-12 text-4xl font-black'>Sponsors</h1>
     </div>
   );
 }
