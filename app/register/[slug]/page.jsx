aae4a530c05422e9711ee1df14b7c789f2fdcff7
Omkar Date <omkardate29@gmail.com>
2024-01-06 14:17:37 +0530

    fix: register -> merch

--- app/register/[slug]/page.jsx
+++ app/register/[slug]/page.jsx
@@ -1,25 +1,5 @@
-import Product from '@/components/product/product';
-import Nav from '@/components/product/nav';
-import './local.css';
-import { productInformation } from '@/product-data';
-import { notFound } from 'next/navigation';
+import { redirect } from 'next/navigation';
 
-const findProduct = (slug) => {
-  return productInformation.find((product) => product.sku === slug);
-};
-
-export default function Page({ params }) {
-  const product = findProduct(params.slug);
-
-  if (!product) {
-    notFound();
-  }
-
-  return (
-    <>
-      <Nav />
-      {/* <Banner /> */}
-      <Product product={product} />
-    </>
-  );
+export default function Page() {
+  return redirect('/merch');
 }
