966c32db5c8680d33dfe8fe2b86eb7e75c5a6a38
Omkar Date <omkardate29@gmail.com>
2024-05-01 14:00:54 +0530

    fix: nav responsiveness

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,11 @@
 import Link from 'next/link';
+import {
+  DropdownMenuLabel,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu';
+import { Menu } from 'lucide-react';
 
 export default function NabBar() {
   const nav = [
@@ -21,8 +28,9 @@ export default function NabBar() {
   ];
 
   return (
-    <nav className='fixed left-0 right-0 top-0 z-50 mx-auto py-4'>
-      <div className='container flex flex-row items-center gap-12 text-start font-bold text-cesa-blue'>
+    <nav className='fixed right-4 top-0 z-50 mx-auto py-4 font-bold text-cesa-blue md:left-0 md:right-0'>
+      {/* Desktop view */}
+      <div className='container hidden flex-row items-center gap-12 md:flex'>
         {nav.map((el, i) => {
           return (
             <Link key={i} href={el.link} className='hover:scale-125'>
@@ -30,6 +38,26 @@ export default function NabBar() {
             </Link>
           );
         })}
+      </div>
+
+      {/* Mobile view */}
+      <div className='block md:hidden'>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Menu className='size-6 stroke-cesa-blue' />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className='mr-2 text-cesa-blue'>
+            {nav.map((el) => {
+              return (
+                <DropdownMenuLabel key={el.link}>
+                  <Link href={el.link}>
+                    {el.name.replace(/[\s]/g, '\u00a0\u00a0')}
+                  </Link>
+                </DropdownMenuLabel>
+              );
+            })}
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </nav>
   );
