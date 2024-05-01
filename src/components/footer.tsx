d92f5c79616b5d085c3cfdddc27de3dc7c9e1769
Omkar Date <omkardate29@gmail.com>
2024-05-01 13:17:29 +0530

    feat: bento grid

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Footer() {
   return (
-    <footer className='relative h-44 w-full'>
+    <footer className='relative mt-24 h-44 w-full'>
       <Image
         src='/images/footer.svg'
         alt='Footer Image'
