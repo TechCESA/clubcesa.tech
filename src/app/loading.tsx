3b99f01e992992645ab065fd267ce4b883dbdb6f
Santosh Phadtare <santoshphadtare1610@gmail.com>
2024-05-30 23:57:31 +0530

    Update loading.tsx

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,4 +1,4 @@
-export default function () {
+export default function loading() {
     return <div className="flex flex-col justify-center items-center w-screen h-screen">
        <p className="font-bold text-2xl text-blue-800">CESA</p>
        <p className="font-normal">Loading bytes...</p>
