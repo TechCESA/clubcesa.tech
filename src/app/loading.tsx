5c069efe93d82ec260285a11bf24260272f4810a
psantosh16 <santoshphadtare1610@gmail.com>
2024-05-30 23:55:28 +0530

    feat: Loading added on main page

---
+++ src/app/loading.tsx
@@ -0,0 +1,6 @@
+export default function () {
+    return <div className="flex flex-col justify-center items-center w-screen h-screen">
+       <p className="font-bold text-2xl text-blue-800">CESA</p>
+       <p className="font-normal">Loading bytes...</p>
+    </div>;
+}
