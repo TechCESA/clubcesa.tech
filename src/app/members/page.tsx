a7d861543f13428ede47dcfeaa2648a5ad9f7758
psantosh16 <santoshphadtare1610@gmail.com>
2024-05-31 02:08:10 +0530

    feat: Firebase Connected & .env added

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,10 +1,22 @@
-import { BackButton } from '@/components/back-button';
+"use client";
+// import { BackButton } from '@/components/back-button';
+import { db } from "@/firebaseConfig";
+import { collection , addDoc } from 'firebase/firestore';
+// import { useState } from 'react';
+
+async function addData() {
+  const docRef = await addDoc(collection(db, "users"), {
+    first: "Ada",
+    last: "Lovelace",
+    born: Math.floor(Math.random() * 100),
+  });
+  console.log("Document written with ID: ", docRef.id);
+}
 
 export default function Page() {
   return (
     <div className='flex min-h-screen flex-col items-center justify-center gap-4'>
-      <h1 className='text-2xl font-bold text-cesa-blue'>Comming Soon...</h1>
-      <BackButton />
+      <h1 onClick={addData}>Add data to firestore</h1>
     </div>
   );
 }
---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_CMS_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_CMS_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_CMS_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_CMS_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_CMS_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_CMS_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_CMS_MEASUREMENT_ID
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
