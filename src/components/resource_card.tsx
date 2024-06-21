87d2bbae9d65c3698d722089147107013c2b1bf0
Omkar Date <omkardate29@gmail.com>
2024-06-22 02:04:46 +0530

    fix: learn page card ui

--- src/components/resource_card.tsx
+++ src/components/resource_card.tsx
@@ -1,5 +1,6 @@
 import { convertTagBtoF } from '@/lib/convert-tags';
 import Link from 'next/link';
+import { Card, CardHeader } from './ui/card';
 
 export default function ResCard({
   title,
@@ -12,22 +13,22 @@ export default function ResCard({
 }) {
   return (
     <Link href={link}>
-      <div className='mx-auto w-full rounded-xl border bg-gray-100 p-2 transition duration-300 ease-in-out hover:border-purple-800/60 hover:bg-purple-50'>
-        <h1 className='flex flex-row items-center justify-between px-2 text-sm font-medium text-primary md:text-base'>
+      <Card className='rounded-xl border transition duration-300 ease-in-out hover:border-purple-800/60 hover:bg-purple-50'>
+        <CardHeader className='flex flex-row items-center justify-between py-2 text-sm font-medium text-primary md:text-base'>
           <span className='truncate'>{convertTagBtoF(title)}</span>
 
           {/* Blink animation */}
           {isNew && (
-            <span className='ml-5 flex flex-row items-center text-xs text-purple-300'>
-              <span className='relative mr-1.5 flex h-2 w-2'>
+            <div className='flex flex-row items-center gap-1 text-xs text-purple-300'>
+              <span className='relative flex size-2'>
                 <span className='absolute inline-flex h-2 w-2 animate-ping rounded-full bg-purple-400 opacity-75'></span>
                 <span className='relative inline-flex h-2 w-2 rounded-full bg-purple-500'></span>
               </span>
               New
-            </span>
+            </div>
           )}
-        </h1>
-      </div>
+        </CardHeader>
+      </Card>
     </Link>
   );
 }
