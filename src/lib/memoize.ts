d6e175ca87cac8728150f61c821ff0f9d98819e0
Omkar Date <omkardate29@gmail.com>
2024-06-22 01:03:28 +0530

    fix: Instead of using an object, using a Map for the cache

--- src/lib/memoize.ts
+++ src/lib/memoize.ts
@@ -1,13 +1,13 @@
 export function memoize<T extends (...args: any[]) => any>(fn: T): T {
-  const cache: { [key: string]: ReturnType<T> } = {};
+  const cache = new Map<string, ReturnType<T>>();
 
   return ((...args: Parameters<T>): ReturnType<T> => {
     const key = JSON.stringify(args);
-    if (cache[key]) {
-      return cache[key];
+    if (cache.has(key)) {
+      return cache.get(key) as ReturnType<T>;
     }
     const result = fn(...args);
-    cache[key] = result;
+    cache.set(key, result);
     return result;
   }) as T;
 }
