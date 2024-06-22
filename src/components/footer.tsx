877fa8bdd9f6040361228d623f4017aef9745747
sanket <sanketbhanuse9@gmail.com>
2024-06-23 01:23:18 +0530

    learn page footer bug fixed

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,11 @@
+"use client"
 import Image from 'next/image';
+import { usePathname } from 'next/navigation';
 
 export default function Footer() {
+  const pathName = usePathname();
   return (
-    <footer className='relative mt-24 h-44 w-full'>
+    <footer className={`relative h-44 w-full ${pathName !== '/learn' && 'mt-24' }`}>
       <Image
         src='/images/footer.svg'
         alt='Footer Image'
