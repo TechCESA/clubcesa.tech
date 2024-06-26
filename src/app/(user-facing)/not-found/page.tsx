c0dba5e17f7e3274a00aa46e6772e5c94de5bfe6
Omkar Date <omkardate29@gmail.com>
2024-06-26 23:36:47 +0530

    feat: not found

---
+++ src/app/(user-facing)/not-found/page.tsx
@@ -0,0 +1,5 @@
+import NotFoundComponent from '@/components/not-found';
+
+export default function NotFound() {
+  return <NotFoundComponent />;
+}
--- src/components/not-found.tsx
+++ src/components/not-found.tsx
@@ -1,22 +1,31 @@
-'use client';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
-import { useRouter } from 'next/navigation';
+function getRandomNumber(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-export default function Not_Found() {
-  const router = useRouter();
-  const goBack = () => router.back();
-
+export default function NotFoundComponent() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-center gap-2'>
-      <h2 className='text-xl font-semibold'>404 Not Found</h2>
-      <p>Sorry, the page you are looking for does not exist.</p>
-      <button
-        // href='/'
-        onClick={goBack}
-        className='mt-4 rounded-md bg-white px-4 py-2 text-sm text-black transition-colors hover:bg-gray-400'
+    <div className='flex min-h-[75vh] flex-col items-center justify-center gap-6'>
+      <h1 className='text-xl font-bold text-cesa-blue'>X_X</h1>
+      <p className='w-1/2 text-center text-xs font-medium sm:w-1/3 sm:text-lg'>
+        {NOT_FOUND_SENT[getRandomNumber(0, 3)]}
+      </p>
+      <Button
+        variant='outline'
+        className='font-semibold text-cesa-blue'
+        asChild
       >
-        Go Back
-      </button>
-    </main>
+        <Link href='/'>Back to Home</Link>
+      </Button>
+    </div>
   );
 }
+
+const NOT_FOUND_SENT = [
+  "You seek a treasure most divine, but alas, that's not on this website of mine!",
+  "Like Dory in Finding Nemo, what you're looking for... just keeps swimming, swimming, swimming!",
+  "My internal search results say... nope, can't find that here. Maybe try googling it?",
+  "Looks like you've reached a dead end... this page doesn't have what you're searching for.",
+];
