aab9665ea0683d3a33e38cc63f82269c532e0e00
Omkar Date <omkardate29@gmail.com>
2024-06-29 12:15:28 +0530

    feat: Note pop up

--- src/components/contribute-form.tsx
+++ src/components/contribute-form.tsx
@@ -9,15 +9,27 @@ import {
   MultiSelectorList,
   MultiSelectorTrigger,
 } from '@/components/extension/multi-selector';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Textarea } from '@/components/ui/textarea';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { useFormState, useFormStatus } from 'react-dom';
 
 export default function ContributeForm({ tags }: { tags: string[] }) {
   const [selectedTags, setSelectedTags] = useState<string[]>([]);
+  const formRef = useRef<HTMLFormElement>(null);
 
   const [formState, formAction] = useFormState(
     contributeResourceAction.bind(null, selectedTags),
@@ -29,6 +41,7 @@ export default function ContributeForm({ tags }: { tags: string[] }) {
 
   return (
     <form
+      ref={formRef}
       action={formAction}
       className='my-8 flex w-full flex-col items-start gap-6 rounded-lg bg-primary-foreground p-4 shadow-md md:my-24 md:max-w-[70%] md:px-12 md:py-8'
     >
@@ -111,26 +124,58 @@ export default function ContributeForm({ tags }: { tags: string[] }) {
         {formState.message && (
           <p className='text-sm text-destructive'>{formState.message}</p>
         )}
-        <SubmitButton />
+        <SubmitButton formRef={formRef} />
       </div>
     </form>
   );
 }
 
-function SubmitButton() {
+function SubmitButton({
+  formRef,
+}: {
+  formRef: React.RefObject<HTMLFormElement>;
+}) {
   const { pending } = useFormStatus();
 
   return (
-    <Button
-      type='submit'
-      disabled={pending}
-      className='flex w-full items-center justify-center bg-cesa-blue'
-    >
-      {pending ? (
-        <span className='animate-bounce text-3xl'>...</span>
-      ) : (
-        'Submit'
-      )}
-    </Button>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button
+          disabled={pending}
+          className='flex w-full items-center justify-center bg-cesa-blue'
+        >
+          {pending ? (
+            <span className='animate-bounce text-3xl'>...</span>
+          ) : (
+            'Submit'
+          )}
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Note</AlertDialogTitle>
+          <AlertDialogDescription className='space-y-2'>
+            <li>{`We value your contribution! Your submission will be reviewed for
+            accuracy and security before it's published.`}</li>
+            <li>
+              {`We'll let you know when
+            it's live through email or you can see it in your profile.`}
+            </li>
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction className='bg-destructive' asChild>
+            <Button
+              type='submit'
+              className='bg-cesa-blue'
+              onClick={() => formRef.current?.requestSubmit()}
+            >
+              Submit
+            </Button>
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 }
