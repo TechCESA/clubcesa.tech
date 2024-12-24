b13d672db85f27bf7c210e1f4ad35eeac0fb61ab
Omkar Date <omkardate29@gmail.com>
2024-12-24 17:27:03 +0530

    refactor: remove unused configuration import in verify-email.ts

--- src/lib/verify-email.ts
+++ src/lib/verify-email.ts
@@ -8,8 +8,7 @@ import { doc, getDoc } from '@firebase/firestore';
 import { render } from '@react-email/components';
 import * as nodemailer from 'nodemailer';
 import Mail from 'nodemailer/lib/mailer';
-import getConfig from 'next/config';
-const { serverRuntimeConfig } = getConfig();
+
 export async function sendVerifyEmail({
   userId,
   resourceId,
