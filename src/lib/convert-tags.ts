c600399f3ce15aee32f175e6aa655e65a8cb8a50
Omkar Date <omkardate29@gmail.com>
2024-12-24 17:40:39 +0530

    fix: handle potential null values in tag conversion functions

--- src/lib/convert-tags.ts
+++ src/lib/convert-tags.ts
@@ -2,24 +2,24 @@ export function convertTagsBtoF(tags: string[]) {
   return tags.map((tag) => {
     return tag
       .split('-')
-      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .map((word) => (word ? word.charAt(0).toUpperCase() + word.slice(1) : ''))
       .join(' ');
   });
 }
 
 export function convertTagsFtoB(tags: string[]) {
   return tags.map((tag) => {
-    return tag.toLowerCase().replace(/\s/g, '-');
+    return tag ? tag.toLowerCase().replace(/\s/g, '-') : '';
   });
 }
 
 export function convertTagBtoF(tag: string) {
   return tag
     .split('-')
-    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .map((word) => (word ? word.charAt(0).toUpperCase() + word.slice(1) : ''))
     .join(' ');
 }
 
 export function convertTagFtoB(tag: string) {
-  return tag.toLowerCase().replace(/\s/g, '-');
+  return tag ? tag.toLowerCase().replace(/\s/g, '-') : '';
 }
